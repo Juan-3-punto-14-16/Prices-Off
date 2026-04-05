1e2e39b3438822db86ebf996ca9f606359b6f95a
xZonitx <168765465+xZonitx@users.noreply.github.com>
2026-04-05 11:07:56 -0600

    No se que pasa porqueria

--- paginaWeb/views/layout.php
+++ paginaWeb/views/layout.php
@@ -23,8 +23,8 @@
     </header>
 
     <nav class=nav_principal>
-      <a href="/">BUSCAR PRODUCTO</a> 
-      | 
+     <a href="/">BUSCAR PRODUCTO</a> 
+     | 
       <a href="/producto/agregar">AGREGAR PRODUCTO</a>
     </nav>
 
