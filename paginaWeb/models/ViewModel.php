10cadcb5e21f66ff481ff9ebd151c8e86640b750
Juan-3-punto-14-16 <juanpablovalladolidramirez@gmail.com>
2026-04-12 13:45:51 -0600

    Modificacion de la API de buscar

--- paginaWeb/models/ViewModel.php
+++ paginaWeb/models/ViewModel.php
@@ -55,7 +55,7 @@ class ViewModel extends ActiveRecord {
                 JOIN catalogo c ON rp.idcatalogo = c.id
                 JOIN ubicacion u ON rp.idubicacion = u.id
                 LEFT JOIN votos v ON rp.id = v.idregistroproducto
-            WHERE c.nombre ILIKE '%" . $nombre . "%'
+            WHERE c.nombre = '$nombre'
             GROUP BY rp.id, c.nombre, u.tienda, u.direccion, u.latitud, u.longitud
             ORDER BY preciounitario
         ";
